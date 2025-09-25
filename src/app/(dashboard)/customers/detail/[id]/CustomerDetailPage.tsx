30aa7aca325a78146c5ffc7cb6266ffdd2f80376
Caner Yayla <caneryaylaa@icloud.com>
2025-09-25 17:19:31 +0300

    refactor: enhance tab navigation with button components in CustomerDetailPage

--- src/app/(dashboard)/customers/detail/[id]/CustomerDetailPage.tsx
+++ src/app/(dashboard)/customers/detail/[id]/CustomerDetailPage.tsx
@@ -2,10 +2,13 @@
 import React from 'react';
 import TotalCountCard from '@/features/Dashboard/TotalCountCard';
 import { ArrowLeftRight, MonitorCog, UsersRound } from 'lucide-react';
-import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
+import { Tabs, TabsContent } from '@/components/ui/tabs';
 import { usePathname, useRouter, useSearchParams } from 'next/navigation';
 import MachinesContent from '@/features/CustomerDetail/tab-contents/MachinesContent';
 import ConnectionsContent from '@/features/CustomerDetail/tab-contents/ConnectionContent';
+import { Card } from '@/components/ui/card';
+import { Button } from '@/components/ui/button';
+import { cn } from '@/lib/utils';
 
 const CustomerDetailPage = () => {
   const router = useRouter();
@@ -63,28 +66,32 @@ const CustomerDetailPage = () => {
         />
       </div>
 
-      <div className="flex w-full">
-        <Tabs value={tab || 'users'} onValueChange={(value) => onChangeHandler(value, 'tab')} className="w-full">
-          <TabsList className="rounded-md">
-            {customerTabs.map((tab, index) => (
-              <TabsTrigger key={index} value={tab.value}>
-                {tab.label}
-              </TabsTrigger>
-            ))}
-          </TabsList>
-          <TabsContent value="users"></TabsContent>
-          <TabsContent value="machines">
-            <MachinesContent customerId={customerId as string} />
-          </TabsContent>
-          <TabsContent value="transitions"></TabsContent>
-          <TabsContent value="connections">
-            <ConnectionsContent customerId={customerId as string} />
-          </TabsContent>
-          <TabsContent value="transitionMethods"></TabsContent>
-          <TabsContent value="payments"></TabsContent>
-          <TabsContent value="settings"></TabsContent>
-        </Tabs>
-      </div>
+      <Tabs value={tab || 'users'} onValueChange={(value) => onChangeHandler(value, 'tab')} className="w-full p-0 m-0 min-h-0">
+        <Card className="w-full flex flex-row flex-wrap p-0 gap-1 md:gap-0 overflow-auto rounded-md">
+          {customerTabs.map((customerTab, index) => (
+            <Button
+              key={index}
+              variant="ghost"
+              className={cn('rounded-md flex-1', tab === customerTab.value && 'bg-accent text-accent-foreground')}
+              onClick={() => onChangeHandler(customerTab.value, 'tab')}
+            >
+              {customerTab.label}
+            </Button>
+          ))}
+        </Card>
+
+        <TabsContent value="users"></TabsContent>
+        <TabsContent value="machines">
+          <MachinesContent customerId={customerId as string} />
+        </TabsContent>
+        <TabsContent value="transitions"></TabsContent>
+        <TabsContent value="connections">
+          <ConnectionsContent customerId={customerId as string} />
+        </TabsContent>
+        <TabsContent value="transitionMethods"></TabsContent>
+        <TabsContent value="payments"></TabsContent>
+        <TabsContent value="settings"></TabsContent>
+      </Tabs>
     </div>
   );
 };
